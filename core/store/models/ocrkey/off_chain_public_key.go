023f90e4fe90b689bbaff37954ca9b5d480eb5bc
spooktheducks <spooktheducks@protonmail.com>
2020-10-30 11:37:45 -0500

    Encode OCR offchain pubkey as hex, not base64

--- core/store/models/ocrkey/off_chain_public_key.go
+++ core/store/models/ocrkey/off_chain_public_key.go
@@ -1,5 +1,18 @@
 package ocrkey
 
-import "crypto/ed25519"
+import (
+	"crypto/ed25519"
+	"encoding/hex"
+)
 
 type OffChainPublicKey ed25519.PublicKey
+
+func (ocpk OffChainPublicKey) MarshalJSON() ([]byte, error) {
+	return []byte(hex.EncodeToString(ocpk)), nil
+}
+
+func (ocpk *OffChainPublicKey) UnmarshalJSON(bs []byte) error {
+	var err error
+	*ocpk, err = hex.DecodeString(string(bs))
+	return err
+}
